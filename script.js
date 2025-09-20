d28235c5a779411cdc426e0a804638ba6a2df49b
JeeThomas <jeeweesin@gmail.com>
2025-09-20 16:57:30 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const links = document.querySelectorAll("nav a");
+    const links = document.querySelectorAll("a[data-page]");
 
     links.forEach(link => {
         link.addEventListener("click", (event) => {
-            event.preventDefault(); // stop default href
+            event.preventDefault(); // stop default #
             const page = link.getAttribute("data-page");
-            window.location.href = page; // navigate
+            window.location.href = page; // go to the page
         });
     });
 });
